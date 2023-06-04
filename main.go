a6c33a16e7106bdf5faeacc7951b2f8cd3251554
minhnhatle104 <lnminh104@gmail.com>
2023-06-05 00:11:41 +0700

    Init project with gin and gorm

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "user:123456@tcp(127.0.0.1:3308)/airbnb?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln("Error in connect db: ", err)
+	}
+	log.Println("Connected to db: ", db)
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run("localhost:8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
